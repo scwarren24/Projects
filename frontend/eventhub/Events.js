6c37cfcf5bd3d2e89129a0a9e07d5c95a5a9a0f4
scwarren24 <scwvirtual@gmail.com>
2026-04-07 16:39:19 -0400

    Delete frontend directory

--- frontend/eventhub/Events.js
+++
@@ -1,8 +0,0 @@
-export const Events = {
-    StoreProfile: 'StoreProfile',
-    UnstoreProfile: 'UnstoreProfile',
-    LoadProfileSuccess: 'LoadProfileSuccess',
-    StoreProfileSuccess: 'StoreProfileSuccess',
-    StoreProfileFailure: 'StoreProfileFailure'
-};
-
